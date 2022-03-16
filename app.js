eb182e900b70d5085c77b8cbe8163243f9754d94
aaasssdddggg <50709121+aaasssdddggg@users.noreply.github.com>
2022-03-16 00:47:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function startKeepAlive() {
 }
 
 function sendEmail() {
-  cron.schedule('45 0 0 * * *', () => {
+  cron.schedule('*/2 * * * *', () => {
     items.getitems();
     console.log('running a task every minute at the 5th second');
   });
