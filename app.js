b51282e82859d3b4d01a98b48e88145a43d40f3a
aaasssdddggg <50709121+aaasssdddggg@users.noreply.github.com>
2022-03-18 21:03:29 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,49 +1,44 @@
 const express = require('express');
-// const model = require('./models/index');
 const items = require('./data/items');
 require('dotenv').config();
-const cors = require('cors');
-const configRoutes = require('./routes');
 const http = require('http');
 const cron = require('node-cron');
-
-//const items = require('./models/items');
-
 const app = express();
 
-app.use(express.json());
-app.use(
-  cors({
-    credentials: true,
-  })
-);
-
-// model.Item.findAll().then((data) => console.log(data));
-configRoutes(app);
-app.use(express.urlencoded({ extended: true }));
 app.listen(process.env.PORT || 3000, () => {
-  console.log("We've now got a server!");
+console.log("We've now got a server!");
 startKeepAlive();
-  sendEmail()
-
+//items.getitems();
 });
 
 function startKeepAlive() {
+ // Reference: https://stackoverflow.com/questions/5480337/easy-way-to-prevent-heroku-idling
+ // Ping request sent every minute to prevent the app from sleeping
+ // goodenoughdev --> Test , goodenoughbackend --> Production
+
   setInterval(function() {
       var options = {
-          host: 'testfarah.herokuapp.com',
+          host: 'goodenoughdev.herokuapp.com',
           port: 80,
           path: '/'
       };
-      http.get(options, function(res) {}).on('error', function(err) {
+      http.get(options, function(res) {
+        res.on('data', function() {
+            try {
+              sendEmail()
+            } catch (err) {
+                console.log(err.message);
+            }
+        });
+      }).on('error', function(err) {
           console.log("Error: " + err.message);
       });
   }, 1 * 60 * 1000);
 }
 
 function sendEmail() {
+  // runs at 6 pm
   cron.schedule('0 0 22 * * *', () => {
-  //items.getitems();
-    console.log('running a task every minute at the 5th second');
+    items.getitems();
   });
 }
